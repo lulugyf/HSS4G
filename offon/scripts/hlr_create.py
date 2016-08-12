e3f4bcfd5d782e4d2736eb276bc698d5d750dc2c
lulugyf <gyf.freedom@gmail.com>
2016-08-12 17:33:16 +0800

    create hlr tables

--- offon/scripts/hlr_create.py
+++ offon/scripts/hlr_create.py
@@ -1,6 +1,8 @@
+#coding=utf-8
 import cx_Oracle
+import sys
 
-
+# 在两个库之间复制基础配置表数据
 def base_data(table=None):
     tbls = '''cbossuserinfo
 cqueryparse
@@ -47,20 +49,8 @@ MSISDNINHLR
         co2.commit()
         print 'rows: ', rowcount
 
-def create_hlr_tables(hlrcode):
-    #co2 = cx_Oracle.connect('offon/ll@xe')
-    co2 = cx_Oracle.connect('offon/Crmpd!06@onoffdb')
-    cur2 = co2.cursor()
-    sqls = file('hlr_tables_list.sql').read()
-    sqls = sqls.replace('${hlrstr}', hlrcode)
-    for sql in sqls.split(';'):
-        sql = clear_sql(sql)
-        if sql == '':
-            continue
-        cur2.execute(sql)
-    co2.close()
 
-# copy config data from from_hlr
+# 建立新的hlrcode的时候， 从一个已有的hlrcode上复制配置数据
 def copy_hlr_config(hlrcode, copyhlr):
     co2 = cx_Oracle.connect('offon/Crmpd!06@onoffdb')
     cur2 = co2.cursor()
@@ -76,6 +66,7 @@ def copy_hlr_config(hlrcode, copyhlr):
         co2.commit()
     co2.close()
 
+# 清理sql语句， 删掉里面以 -- 开头的行
 def clear_sql(sql):
     arr = sql.split('\n')
     arr1 = []
@@ -86,6 +77,25 @@ def clear_sql(sql):
     sql = "\n".join(arr1)
     return sql.strip()
 
+# 执行建表语句， 对于重复执行的报错忽略
+def create_sql_exec(cur, sql):
+    try:
+        cur.execute(sql)
+    except Exception, e:
+        s = str(e)
+        if s.find('name is already used by an existing object') >= 0:
+            return True
+        if s.find('such column list already indexed') >= 0:
+            return True
+        if s.find(' table can have only one primar') >= 0:
+            return True
+        print e
+        print sql
+        return False
+    return True
+
+
+# 创建开关机的基本配置表
 def create_base_tables():
     # co2 = cx_Oracle.connect('offon/ll@xe')
     co2 = cx_Oracle.connect('offon/Crmpd!06@onoffdb')
@@ -95,19 +105,28 @@ def create_base_tables():
         sql = clear_sql(sql)
         if sql == '':
             continue
-        try:
-            cur2.execute(sql)
-        except Exception,e:
-            s = str(e)
-            if s.find('name is already used by an existing object') >= 0:
-                continue
-            if s.find('such column list already indexed') >= 0:
-                continue
-            if s.find(' table can have only one primar') >= 0:
-                continue
-            print e
-            print sql
+
+        if not create_sql_exec(cur2, sql):
             break
+    cur2.close()
+    co2.close()
+
+# 创建hlr的指令数据分表 sndcmdday??? soprcmd??? ssubcmd??? dusrdata???
+def create_hlr_tables(hlrcodes):
+    #co2 = cx_Oracle.connect('offon/ll@xe')
+    co2 = cx_Oracle.connect('offon/Crmpd!06@onoffdb')
+    cur2 = co2.cursor()
+    for hlrcode in hlrcodes.split(","):
+        hlrcode = hlrcode.strip()
+        sqls = file('hlr_tables_list.sql').read()
+        sqls = sqls.replace('${hlrstr}', hlrcode)
+        for sql in sqls.split(';'):
+            sql = clear_sql(sql)
+            if sql == '':
+                continue
+            if not create_sql_exec(cur2, sql):
+                break
+    cur2.close()
     co2.close()
 
 
@@ -115,7 +134,7 @@ if __name__ == '__main__':
     # create_base_tables()
     #base_data()
     #base_data('cHlrCfgInfo')
-    #create_hlr_tables('hei')
+    create_hlr_tables(sys.argv[1])
     #create_hlr_tables('scx')
     #copy_hlr_config('scx', 'hei')
     pass
